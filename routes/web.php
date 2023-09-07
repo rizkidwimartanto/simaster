53f6f90e3131a685ca28334ee0929f0cc7109283
rizkidwimartanto <rizkidwimartanto@gmail.com>
2023-09-07 21:13:12 +0700

    menambahkan tampilan register & login

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login', ['title' => 'Login']);
+});
+Route::get('/registrasi', function(){
+    return view('register', ['title' => 'Registrasi']);
 });
