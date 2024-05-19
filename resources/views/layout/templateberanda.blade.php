d7741227832b0cf82ee15bf0848aae08605a5d5f
rizkidwimartanto <rizkidwimartanto@gmail.com>
2024-05-20 06:39:50 +0700

    kembalikan judul website lagi

--- resources/views/layout/templateberanda.blade.php
+++ resources/views/layout/templateberanda.blade.php
@@ -15,7 +15,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Tes Lagi</title>
+    <title>{{ $title }}</title>
     <!-- CSS files -->
     <link href="assets_template/dist/css/tabler.min.css?1684106062" rel="stylesheet" />
     <link href="assets_template/dist/css/tabler-flags.min.css?1684106062" rel="stylesheet" />
